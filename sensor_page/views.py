4c341faca807e8b233d5777195da2627f0e09190
Owen Kwon <ohkeunow@qti.qualcomm.com>
2015-02-15 18:58:22 +0900

    modified cronjob period to 5min.

--- sensor_page/views.py
+++ sensor_page/views.py
@@ -566,7 +566,7 @@ def dynamic_png(sensor, display_fmt, time_offset):
     date_max = date_max.replace(tzinfo=None)
     date_min = date_min.replace(tzinfo=None)
 
-    warning_delta = datetime.timedelta(seconds=sensor.sensor_node.warning_period)
+    warning_delta = datetime.timedelta(seconds=sensor.sensor_node.warning_period + 60)
 
     date_max += tz.utcoffset(date_max)
     date_min += tz.utcoffset(date_min)
